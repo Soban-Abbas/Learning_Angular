b797ce93f416cc902717ac4994e8fccca0839722
Soban Abbas <sobanabbass@gmail.com>
2025-10-07 21:39:23 +0500

    learning templete driven forms

--- src/app/day2/day2.ts
+++ src/app/day2/day2.ts
@@ -1,11 +1,27 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-day2',
-  imports: [],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './day2.html',
   styleUrl: './day2.css'
 })
 export class Day2 {
 
+//templete driven form 
+userreg : any ={
+  fullname:'',
+  email:'',
+  phonenumber:'',
+  city:'',
+  zip:'',
+  isacceptTerms:false
+}
+details:any;
+onsubmit(){
+  debugger;
+this.details=this.userreg;
+}
 }
